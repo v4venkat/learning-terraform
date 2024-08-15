46605f7f3051d9441cf9cabd792941753d46b2ac
Venkat-Adusumalli <73272870+v4venkat@users.noreply.github.com>
2024-08-14 22:44:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
-data "aws_ami" "app_ami" {
-  most_recent = false
-
-  filter {
-    name   = "image-id"
-    values = ["ami-0ae8f15ae66fe8cda"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = "ami-0ae8f15ae66fe8cda"
   instance_type = "t2.micro"
 
   tags = {
